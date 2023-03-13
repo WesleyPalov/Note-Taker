37a94c3c4f01b066327f32a2e04839b672b79817
WesleyCod <wesleypalov@yahoo.com>
2023-03-13 15:39:23 -0700

    new try2

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) =>
 
 // GET Route for note taker page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/pages/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 // Promise version of fs.readFile
